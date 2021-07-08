84e103b5c01e8b4bb969c1bcacaa624e89169329
Jeffery Mayhall <jzm0162@auburn.edu>
2021-07-07 23:13:03 -0500

    Filling in main window methods

--- CurlingLeague/GUI/main_window.py
+++ CurlingLeague/GUI/main_window.py
@@ -1,4 +1,7 @@
 import sys
+
+from PyQt5.QtWidgets import QMessageBox, QFileDialog, QInputDialog
+
 from CurlingLeague.league_database import LeagueDatabase
 from PyQt5 import uic, QtWidgets
 
@@ -10,6 +13,7 @@ class MainWindow(QtBaseWindow, UI_MainWindow):
         super().__init__(parent)
         self.setupUi(self)
         self.db = LeagueDatabase.instance()
+        self.update_ui()
         self.load_database_button.clicked.connect(self.load_button_clicked)
         self.save_database_button.clicked.connect(self.save_button_clicked)
         self.add_league_button.clicked.connect(self.add_league_clicked)
@@ -18,23 +22,41 @@ class MainWindow(QtBaseWindow, UI_MainWindow):
         self.import_league_button.clicked.connect(self.import_league_clicked)
         self.export_league_button.clicked.connect(self.export_league_clicked)
 
+    def update_ui(self):
+        self.leagues_listwidget.clear()
+        for l in self.db.leagues:
+            self.leagues_listwidget.addItem(l)
+
     def load_button_clicked(self):
-        pass
+        file = QFileDialog().exec()
+        if file:
+            self.db.load(file)
+        self.update_ui()
 
     def save_button_clicked(self):
         pass
 
     def add_league_clicked(self):
         self.db.add_league(self.new_league_lineedit.text(), self.db.next_oid)
+        self.update_ui()
 
     def edit_league_clicked(self):
         pass
 
     def delete_league_clicked(self):
-        pass
+        dialog = QMessageBox(QMessageBox.Icon.Information,
+                             "Confirm Delete",
+                             "The selected league will be deleted!",
+                             QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel)
+        result = dialog.exec()
+        if result == QMessageBox.StandardButton.Ok:
+            print("Approved")
 
     def import_league_clicked(self):
-        pass
+        dialog = QInputDialog()
+        result = dialog.exec()
+        file = QFileDialog().exec()
+        self.db.import_league(result, file)
 
     def export_league_clicked(self):
         pass
